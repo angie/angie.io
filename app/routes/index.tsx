1e8a1e7a9154507ba2c8c566a1e1b7a722b3d38c
Angie Merryweather <6738119+angie@users.noreply.github.com>
2022-05-08 11:51:15 +0100

    pink 👍

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,7 +2,9 @@ export default function Index() {
   return (
     <div className="h-screen flex items-center">
       <main className="px-[50px] md:px-[100px]">
-        <h1 className="text-6xl font-bold">hiya 👋</h1>
+        <div className="bg-[#fe7cb9] px-1 py-3">
+          <h1 className="text-6xl font-bold underline">hiya 👋</h1>
+        </div>
       </main>
     </div>
   )
